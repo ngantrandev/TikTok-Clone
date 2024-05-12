013dbf2ebc30b2b51aefb6fca7c9e6f20be88051
Ngan Tran <78363797+ngantrandev@users.noreply.github.com>
2024-05-12 16:58:39 +0700

    config layout loading mechanism

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,20 @@
+// pages
 import Home from '../components/pages/Home';
 import Fowllowing from '../components/pages/Following';
 import Profile from '../components/pages/Profile';
+import Upload from '../components/pages/Upload';
+import Search from '../components/pages/Search';
+
+// layouts
+import { HeaderOnly } from '../components/Layout';
 
 const publicRoutes = [
   { path: '/', component: Home },
+  { path: '/home', component: Home },
   { path: '/following', component: Fowllowing },
   { path: '/profile', component: Profile },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
 ];
 
 const privateRoutes = [];
